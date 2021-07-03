a4b615dbef9e4a0c8dd092ee6515dc420a22734e
catrielzz <estradayoel@gmail.com>
2021-07-03 19:39:02 -0300

    chatService updated

--- nodejs/server/service/chatService.js
+++ nodejs/server/service/chatService.js
@@ -17,27 +17,7 @@ const functions = {
         return new Promise((resolve, reject) => {
             resolve(chatDTO.listChatsFromDb(userId));
         });
-    },
-    // update: function updateMessage(id, message) {
-    //     return new Promise(async (resolve, reject) => {
-    //         if(!id || !message){
-    //             reject('[Message Service] Invalid Data');
-    //             return false;
-    //         }
-    //         const result = await store.updateMessageFromDb(id, message);
-    //         resolve(result);
-    //     });
-    // }, 
-    // delete: function deleteMessage(id){
-    //     return new Promise(async (resolve, reject) => {
-    //         if(!id){
-    //             reject('[Message Service] Invalid Data');
-    //             return false;
-    //         }
-    //         const result = await store.deleteMessageFromDb(id);
-    //         resolve(result);
-    //     });
-    // }
+    }
 }
 
 export default functions;
